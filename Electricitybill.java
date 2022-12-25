513bcebe7d961ff3fb236c960bfa0c6045bc2abd
ShifaniFrancis <94130152+ShifaniFrancis@users.noreply.github.com>
2022-12-25 11:20:07 +0530

    Add files via upload

---
+++ Electricitybill.java
@@ -0,0 +1,82 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package electricitybill;
+
+/**
+ *
+ * @author 91822
+ */
+import java.util.*;
+
+class threadclass extends Thread{   // inheritence and multithreading
+    public void run(){
+        for(int i=5;i>=0;i--){
+            System.out.println("Your bill will be loaded in "+i+"  seconds....");
+            try{               // exception handling
+                Thread.sleep(500);
+            }
+            catch(Exception e){
+                System.out.println(e);
+            }
+        }
+    }
+}
+    
+    
+
+interface bill{      //interface
+    public void calcbill() throws negativeunits, InterruptedException;
+}
+
+
+class EB implements bill{   //implementing interface
+    public void calcbill() throws negativeunits,InterruptedException{
+        int units;
+ 
+	double billpay=0;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter number of units consumed:");
+        units=sc.nextInt();
+        if(units<0){
+            throw new negativeunits("Negative units nit allowed here");
+        }
+        else if(units<100)
+	 {
+	      billpay=units*1.20;
+	  }
+	else if(units<300)
+	 {
+	   billpay=100*1.20+(units-100)*2;
+	 }
+	else if(units>300)
+	{
+	    billpay=100*1.20+200*2+(units-300)*3;
+	}
+        threadclass t1=new threadclass();
+        t1.start();
+        t1.join();
+	System.out.println("Bill to pay : " + billpay); 
+      } 
+    }
+
+class negativeunits extends Exception{
+    public negativeunits(String str){
+        super(str);
+    }
+}
+
+public class Electricitybill {
+
+    public static void main(String[] args)throws InterruptedException {
+        EB eb=new EB();
+        try{
+        eb.calcbill();
+        }
+        catch(negativeunits e){
+            System.out.println(e);
+        }
+    }
+    
+}
